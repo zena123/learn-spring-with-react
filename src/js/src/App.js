945d909cf469d73d60feba4dff1322041048a282
zena123 <zinab.jom3a@gmail.com>
2023-12-23 00:24:43 +0100

    add ant design package table

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,4 +1,5 @@
 
+import { Table } from 'antd';
 import {getAllStudents} from './client.js'
 import React, { Component } from 'react';
 
@@ -22,17 +23,42 @@ class  App extends Component {
   render(){
     const {students} = this.state;
     if (students && students.length) {
+      const columns = [
+        { title: 'Student Id',
+        dataIndex: 'studentId',
+        key:'studentId'
+        },
+        { title: 'First Name',
+        dataIndex: 'firstName',
+        key:'firstName'
+        },
+        { title: 'Last Name',
+        dataIndex: 'lastName',
+        key:'lastName'
+        },
+        { title: 'Email',
+        dataIndex: 'email',
+        key:'email'
+        },
+        { title: 'Gender',
+        dataIndex: 'gender',
+        key:'gender'
+        }
+      ];
+      return (<Table dataSource={students}
+       columns={columns}
+        rowKey='studentId'/>);
       // callback
-      return students.map((student, index)=>{
-        return (
-          <div key={index}>
-            <h2>{student.studentId}</h2>
-            <p>{student.firstName}</p>
-            <p>{student.lastName}</p>
-            <p>{student.gender}</p>
-          </div>
-        )
-      })
+      // return students.map((student, index)=>{
+      //   return (
+      //     <div key={index}>
+      //       <h2>{student.studentId}</h2>
+      //       <p>{student.firstName}</p>
+      //       <p>{student.lastName}</p>
+      //       <p>{student.gender}</p>
+      //     </div>
+      //   )
+      // })
 
     }
     return (
