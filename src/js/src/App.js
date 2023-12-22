c8ce40685740126f7536c74cc44ce95b52aed575
zena123 <zinab.jom3a@gmail.com>
2023-12-22 23:13:13 +0100

    fetch students in react with unfetch

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import {getAllStudents} from './client.js'
+import React, { Component } from 'react';
+
+class  App extends Component {
+  state = {
+    students: []
+  }
+  componentDidMount(){
+    this.fetchstudents();
+  }
+  fetchstudents = () =>{
+    getAllStudents()
+    .then(res =>res.json()
+    .then(students =>{
+      this.setState({
+        students: students
+      })
+    }));
+  
+  }
+  render(){
+    const {students} = this.state;
+    if (students && students.length) {
+      // callback
+      return students.map((student, index)=>{
+        return (
+          <div key={index}>
+            <h2>{student.studentId}</h2>
+            <p>{student.firstName}</p>
+            <p>{student.lastName}</p>
+            <p>{student.gender}</p>
+          </div>
+        )
+      })
+
+    }
+    return (
+      <h1> no students found</h1>
   );
+  }
 }
 
 export default App;
