58a1c68e7ad1b86310ff3113c355db2d611f3687
zena123 <zinab.jom3a@gmail.com>
2023-12-23 23:54:18 +0100

    fix typo in studentdataSource

--- src/main/java/mycode/demo2/student/StudentDataAccessService.java
+++ src/main/java/mycode/demo2/student/StudentDataAccessService.java
@@ -17,19 +17,23 @@ public class StudentDataAccessService {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<Student> selectAllStudents(){
-        String sql = "" + "SELECT student_id, first_name, last_name, email, gender, from student";                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 lk";
-        
-        List<Student> students = jdbcTemplate.query(sql, (ResultSet resultSet, i) -> {
+    List<Student> selectAllStudents(){
+        String sql = "" + "SELECT student_id, first_name, last_name, email, gender from student"; 
+
+        return jdbcTemplate.query(sql, ( resultSet, i) -> {
             String idStr = resultSet.getString("student_id");
             UUID studentId = UUID.fromString(idStr);
             String firstName = resultSet.getString("first_name");
             String lastName = resultSet.getString("last_name");
             String email = resultSet.getString("email");
             String genderStr = resultSet.getString("gender").toUpperCase();
-            return new Student(studentId, firstName, lastName, email, Student.Gender.valueOf(genderStr));
+            return new Student(studentId,
+             firstName,
+              lastName, 
+              email, 
+              Student.Gender.valueOf(genderStr)
+              );
         });
-        return students;
     }
     
 }
